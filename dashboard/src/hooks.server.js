1e67fa4ba8902230266a05ba2c939ba40c4a55dc
Ruben Uijtdewilligen <ruben@uijtdewilligenbv.nl>
2024-01-29 16:07:26 +0100

    Connect to PocketBase

---
+++ dashboard/src/hooks.server.js
@@ -0,0 +1,17 @@
+import PocketBase from 'pocketbase';
+import { env } from '$env/dynamic/public';
+
+export const handle = async ({ event, resolve }) => {
+  event.locals.pb = new PocketBase(env.PUBLIC_POCKETBASE_URL || 'https://localhost:8090');
+  event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+
+  if (event.locals.pb.authStore.isValid) {
+    event.locals.user = structuredClone(event.locals.pb.authStore.model);
+  } else {
+    event.locals.user = undefined;
+  }
+
+  const response = await resolve(event);
+  response.headers.set('set-cookie', event.locals.pb.authStore.exportToCookie());
+  return response;
+};
